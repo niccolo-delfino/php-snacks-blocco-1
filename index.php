4059473b2cedc320a5791e478e433d4610abeec7
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-25 16:38:18 +0100

    Array provvisoria

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
+<!-- PHP Snack 1:
+Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di
+basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di
+casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55 - 60 -->
+
 <?php
+$matches = [
+        [
+            "SqudraCasa" => "Olimpia Milano",
+            "SquadraOspite" => "Virtus Bologna",
+            "PuntiCasa" => 120,
+            "PuntiOspite" => 131
+        ],
+        [
+            "SqudraCasa" => "Varese",
+            "SquadraOspite" => "Pall. Cantù",
+            "PuntiCasa" => 99,
+            "PuntiOspite" => 102
+        ],
+        [
+            "SqudraCasa" => "V.L. Pesaro",
+            "SquadraOspite" => "Reyes Venezia",
+            "PuntiCasa" => 110,
+            "PuntiOspite" => 122
+        ]
+    ]
 
 ?>
 
